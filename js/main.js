d370ed9c0d9f0bebc90230fdaac865c64ecd9316
michaelg4u101 <michaelg4u101@gmail.com>
2026-05-05 17:33:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+// פס התקדמות בגלילה
+window.onscroll = function() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+};
 
+// הפעלת מוזיקת רקע
+const music = document.getElementById("bgMusic");
+const btn = document.getElementById("musicBtn");
+
+if (btn) {
+    btn.addEventListener("change", function() {
+        if (this.checked) {
+            music.play().catch(e => console.log("צריך אינטראקציה ראשונית להפעלת סאונד"));
+        } else {
+            music.pause();
+        }
+    });
+}
